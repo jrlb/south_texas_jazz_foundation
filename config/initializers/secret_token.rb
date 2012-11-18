e713973f8df42e4850b2c5a709a484b18b35716d
Robert Boone <robert@jrlbconsulting.com>
2012-11-18 14:13:36 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SouthTexasJazzFoundation::Application.config.secret_token = '7721ece2212036e777456f10c74c61ca42dc6d45a6101af4610ac916bc1907ca23409fda98dfcdf30be8f7edf01f19a8dd561fdcbd4dfbd247aeadd33fa38183'
