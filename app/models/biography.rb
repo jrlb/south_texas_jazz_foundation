faa480459ba94839728593ecc78309c67563f5b5
Robert Boone <robert@jrlbconsulting.com>
2013-02-18 19:50:12 -0600

    Remove sign up links and pages

--- app/models/biography.rb
+++ app/models/biography.rb
@@ -1,9 +1,4 @@
 class Biography < ActiveRecord::Base
   attr_accessible :biography, :first_name, :instrument, :last_name
 
-  if Rails.env.production?
-    devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
-  else
-    devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :registerable 
-  end
 end
