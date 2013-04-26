8e49ee745e22ceedfecfa7c418782c3d9e46ee8e
Robert Boone <robert@rlb3.com>
2013-04-26 10:16:08 -0500

    Add Presenters for heroku production

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module SouthTexasJazzFoundation
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-    config.autoload_paths += %W(#{config.root}/app/Presenters)
+    config.autoload_paths += %W(#{config.root}/app/Presenters /app/Presenters)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
