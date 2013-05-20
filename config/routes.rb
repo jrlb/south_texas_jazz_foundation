42c3681a41f562f0aab2559338e6869b2227898c
Robert Boone <robert@rlb3.com>
2013-05-20 17:17:37 -0500

    Update bioagraphies#show to band_memebers#show so the nav select will work.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SouthTexasJazzFoundation::Application.routes.draw do
   resources :biographies
-  get "/band_members" => "biographies#index", :as => 'band_members'
+  get '/band_members' => 'biographies#index', :as => 'band_members'
+  get '/band_members/:id' => 'biographies#show', :as => 'band_member'
 
   devise_for :members
 
