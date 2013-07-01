6b73ffca1cbf52bc08e2ba067ddf2fffbbdaee64
Robert Boone <robert@rlb3.com>
2013-07-01 08:32:00 -0500

    Small updates

--- app/models/biography.rb
+++ app/models/biography.rb
@@ -1,11 +1,11 @@
 class Biography < ActiveRecord::Base
   attr_accessible :biography, :first_name, :instrument, :last_name, :section
 
-  scope :conductors, -> { where(section: 'conductor').order("last_name") }
-  scope :saxes, -> { where(section: 'sax').order("last_name") }
-  scope :bones, -> { where(section: 'bone').order("last_name") }
-  scope :trumpets, ->{ where(section: 'trumpet').order("last_name") }
-  scope :rhythm, -> { where(section: 'rhythm').order("last_name") }
+  scope :conductors, -> { where(section: 'conductor').order('last_name') }
+  scope :saxes,      -> { where(section: 'sax').order('last_name')       }
+  scope :bones,      -> { where(section: 'bone').order('last_name')      }
+  scope :trumpets,   -> { where(section: 'trumpet').order('last_name')   }
+  scope :rhythm,     -> { where(section: 'rhythm').order('last_name')    }
 
   def full_name
     "#{first_name} #{last_name}"
