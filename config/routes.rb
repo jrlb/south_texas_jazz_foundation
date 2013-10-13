91fd3f5b0bf0082a2c016d85b16e64a177268c1b
Robert Boone <robert@rlb3.com>
2013-10-13 14:23:53 -0500

    Add route for hook

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SouthTexasJazzFoundation::Application.routes.draw do
   resources :press_releases
   resources :donations
+  post 'donations/hook', to: 'donations#hook'
   resources :biographies
   get '/band_members' => 'biographies#index', :as => 'band_members'
   get '/band_members/:id' => 'biographies#show', :as => 'band_member'
